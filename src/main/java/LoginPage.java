18cd203b981a23daf574a0a4a2c7cac3136018cc
Egor Koshelev <ekoshelev@ibsys.ru>
2026-01-08 17:54:46 +0300

    add allure report

---
+++ src/main/java/LoginPage.java
@@ -0,0 +1,8 @@
+import org.openqa.selenium.By;
+
+public class loginPage {
+    private final By loginToAccountButton = By.xpath("//button[text()='Войти в аккаунт']");
+    private final By emailInput = By.xpath("//input[@type='text']");
+
+
+}
